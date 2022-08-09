cbbe00892bc0c62ae9ad21ec01f4f556544840c4
sserditov <108936913+sserditov@users.noreply.github.com>
2022-08-09 15:38:30 +0500

    Задача4

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b | a > c | b > c)
 {
-    Console.WriteLine("Максимальное число " + a);
+    Console.WriteLine("Максимальное число ");
 }
 else
 {
-    Console.WriteLine("Максимальное число " + b);
+    Console.WriteLine(" ");
 }
